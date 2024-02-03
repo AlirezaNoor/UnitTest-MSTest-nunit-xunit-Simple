810341bb4cd3bce5fb9a5e027f44ed987111c2f9
shahab-i317 <shahab-i317@sdit.in>
2024-02-03 12:57:32 +0330

    SparkyNunitTest-Add test

---
+++ SparkyNunitTest/CalculatorNunitTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using Sparky;
+
+namespace SparkyNunitTest;
+[TestFixture]
+public class CalculatorNunitTest
+{
+    [Test]
+    public void Calulate_Two_number_Toghether()
+    {
+        //Aarrnge
+        Calculator cal = new();
+        //Act
+        int result= cal.AddCalculater(10,20);
+        //Assert
+        Assert.AreEqual(30 ,result);
+    }
+}
