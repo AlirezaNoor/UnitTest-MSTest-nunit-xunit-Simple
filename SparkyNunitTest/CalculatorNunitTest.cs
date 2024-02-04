8cc658a05b06f605ea001989713a7d7491266815
shahab-i317 <shahab-i317@sdit.in>
2024-02-04 11:21:20 +0330

    use TestCase-new method-version3

--- SparkyNunitTest/CalculatorNunitTest.cs
+++ SparkyNunitTest/CalculatorNunitTest.cs
@@ -49,19 +49,32 @@ public class CalculatorNunitTest
         Assert.That(result, Is.EqualTo(true));
         Assert.IsTrue(result);
     }
+
     [Test]
-    [TestCase(10,ExpectedResult = false)]
-    [TestCase(11,ExpectedResult = true)]
-    public bool Calculate_Input_OddNummber_True( int a)
+    [TestCase(10, ExpectedResult = false)]
+    [TestCase(11, ExpectedResult = true)]
+    public bool Calculate_Input_OddNummber_True(int a)
     {
         //Aarrnge
         Calculator cal = new();
 
         //Act
         bool result = cal.OddNumberCalculate(a);
-        
-        return result;
 
+        return result;
+    }
+
+    [Test]
+    [TestCase(5.4,10.5)]
+    public void Calculate_TWo_Double_Number_Together(double a , double b)
+    {
+        //Aarrnge
+        Calculator cal = new();
+
+        //Act
+        var  result = cal.AddDoubleCalculator(a,b);
+        
+        //Assert
+        Assert.AreEqual(15.9,result,.1);
     }
 }
-
