1654a11bc5aa0a43c4462eb719396aa87f5fe289
shahab-i317 <shahab-i317@sdit.in>
2024-02-04 09:38:58 +0330

    use TestCase

--- SparkyNunitTest/CalculatorNunitTest.cs
+++ SparkyNunitTest/CalculatorNunitTest.cs
@@ -18,29 +18,33 @@ public class CalculatorNunitTest
     }
 
     [Test]
-    public void Calculate_For_Input_Not_odd_number_OutPut_False()
+    [TestCase(10)]
+    [TestCase(200)]
+    public void Calculate_For_Input_Not_odd_number_OutPut_False(int a)
     {
         //Aarrnge
         Calculator cal = new();
-        
+
         //Act
-        bool result = cal.OddNumberCalculate(10);
-        
+        bool result = cal.OddNumberCalculate(a);
+
         //Assert
-        
-        Assert.That(result,Is.EqualTo(false));
+
+        Assert.That(result, Is.EqualTo(false));
     }
 
     [Test]
-    public  void Calculate_Input_OddNummber_OutPut_True()
+    [TestCase(11)]
+    [TestCase(21)]
+    public void Calculate_Input_OddNummber_OutPut_True(int a)
     {
         //Aarrnge
         Calculator cal = new();
-        
+
         //Act
-        bool result = cal.OddNumberCalculate(11);
-        
+        bool result = cal.OddNumberCalculate(a);
+
         //Assert
-        Assert.That(result,Is.EqualTo(true));
+        Assert.That(result, Is.EqualTo(true));
     }
 }
