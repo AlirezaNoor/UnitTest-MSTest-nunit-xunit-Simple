63fda6f2f332c4833dbf6bbb2ed12ce8f7c471e4
shahab-i317 <shahab-i317@sdit.in>
2024-02-05 09:04:08 +0330

    this is  good idea

--- Sparky/StringSparky.cs
+++ Sparky/StringSparky.cs
@@ -2,8 +2,16 @@
 
 public class StringSparky
 {
-    public string SayHellow(string name ,string lastname)
+    public string SayHellow(string name, string lastname)
     {
         return $"helllow {name} {lastname}";
+    }
+
+    public string GenerateName { get; set; }
+
+    public string FullName(string name, string lastname)
+    {
+        GenerateName = $"helllow {name} {lastname}";
+        return GenerateName;
     }
 }
