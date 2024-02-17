f4df6a5354ce4ce071658aca2bf2cb0e9bc80d90
shahab-i317 <shahab-i317@sdit.in>
2024-02-17 11:31:15 +0330

    this is an other Tdd Project I Will added to git repository

---
+++ TDD-Fizz-Buzz/FizzBuzzClass.cs
@@ -0,0 +1,34 @@
+﻿namespace TDD_Fizz_Buzz;
+
+public class FizzBuzzClass
+{
+    public List<string> FizzBuzzMethod(int a)
+    {
+        List<string> result = new List<string>();
+        for (int i = 1; i <= a; i++)
+        {
+            if (i%15==0)
+            {
+                result.Add("FizzBuzz");  
+            }
+            else if (i % 3 == 0)
+            {
+                result.Add("Fizz");
+            }
+
+
+            else if (i % 5 == 0)
+            {
+                result.Add("Buzz");
+            }
+            else
+            {
+                result.Add(i.ToString());
+            }
+
+          
+        }
+
+        return result;
+    }
+}
